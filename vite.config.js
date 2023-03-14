f80517e73f8abc1a2aac9a768403856162aa9ae7
Pawar <Nilesh.Pawar@saint-gobain.com>
2023-03-14 21:48:19 +0530

    new changes realted to path

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://nileshpawars.github.io/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
